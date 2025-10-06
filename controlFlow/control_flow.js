20a28c2bc2f8c8fe0f68496a2c983d60c0e8a802
daniel <daniel.george94@gmail.com>
2025-10-06 16:33:34 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -4,6 +4,8 @@ let isLoggedIn = true;
 let userMessage;
 let userType = "subscriber";
 let userCategory;
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 if (userRole === "admin") {
     accessLevel = "Full access granted";
